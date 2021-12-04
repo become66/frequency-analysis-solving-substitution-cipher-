bd2f6e93ad1eb5a94f8800c1c1c273630a18045a
become66 <a0981695482@gmail.com>
2021-12-05 00:25:29 +0800

    finish (pass the 3 testcases on the server)

--- project1.cpp
+++ project1.cpp
@@ -12,12 +12,11 @@
 #include <ctime>   
 
 #define ull unsigned long long
+#define mutatedMaxWidth 15
+#define repeatTime 3000
+#define processingTime 20
 
 using namespace std;
-
-
-
-
 
 unordered_map<string, double> getQuadrigram(){
     
@@ -56,11 +55,11 @@ void getNewQuastr(string &pre, char nextC){
     pre.push_back(nextC);
 }
 
-double getScore(const string &str, unordered_map<string, double> &Quadrigram){
+double getScore(const string &plainText, const unordered_map<string, double> &Quadrigram){
     double score = 0;
-    string quadrStr = "." + str.substr(0,3); //"." is dummy
-    for(size_t i = 3; i < str.size(); ++i){
-        getNewQuastr(quadrStr, str[i]);
+    string quadrStr = "." + plainText.substr(0,3); //"." is dummy
+    for(size_t i = 3; i < plainText.size(); ++i){
+        getNewQuastr(quadrStr, plainText[i]);
         unordered_map<string,double>::const_iterator it = Quadrigram.find (quadrStr);
         if ( it == Quadrigram.end() ){ // not find
             score += log10(0.01/4224127913); // 4224127913 is the total count of Quadrigram
@@ -74,7 +73,7 @@ double getScore(const string &str, unordered_map<string, double> &Quadrigram){
 
 string readCypherText(map<char,ull> &countMap){
     string cypherText;
-    std::ifstream ifs("test.txt", std::ios::in);
+    std::ifstream ifs("cypherText.txt", std::ios::in);
     if (!ifs.is_open()) {
         cout << "Failed to open file.\n";
     } 
@@ -110,7 +109,7 @@ string getsortedLetterString(map<char,ull> &countMap){
 }
 
 unordered_map<char, char> getDecryptDictionary(const string &sortedLetterString,const string &mappedString){
-    cout<<sortedLetterString.size()<<","<< mappedString.size()<<"(should both be 26)\n";
+    // cout<<sortedLetterString.size()<<","<< mappedString.size()<<"(should both be 26)\n";
     unordered_map<char, char> decryptDictionary;
     for(size_t i = 0; i < sortedLetterString.size(); ++i){
         decryptDictionary[sortedLetterString[i]] = mappedString[i];
@@ -126,7 +125,7 @@ string decrypt(const string &cypherText, const unordered_map<char, char> &decryp
     return plainText;
 }
 
-void getNewMappedString(string &mappedString, int maxWidth){
+string getMutatedMappedString(const string &mappedString, int maxWidth){
     try {
         if (maxWidth < 1 || maxWidth > 26) {
             throw "maxWidth should > 0 && < 26!";
@@ -135,6 +134,8 @@ void getNewMappedString(string &mappedString, int maxWidth){
     catch (const char* message) {
         std::cout << message << std::endl;
     }
+    
+    string mutatedMappedString = mappedString;
     //take maxWidth = 6 for example
     int width, pos; 
     if(maxWidth == 1){
@@ -146,7 +147,42 @@ void getNewMappedString(string &mappedString, int maxWidth){
 
     pos = rand()%(26 - width); // if width = 6 =>  pos = 0~19; if width = 1 => pos = 0~24
     int firstIdx = pos, secondIdx = pos + width;
-    swap(mappedString[firstIdx], mappedString[secondIdx]);
+    swap(mutatedMappedString[firstIdx], mutatedMappedString[secondIdx]);
+    return mutatedMappedString;
+}
+
+pair<double,pair<string, string>> getNearOptimalDecrytionKey(const unordered_map<string, double> &Quadrigram, const string &sortedLetterString , const string &cypherText){
+    double highestScore = 0, mutatedScore = 0;
+    string  mappedString, mutatedMappedString; 
+    unordered_map<char, char> decryptDictionary, mutatedDecryptDictionary;
+    string plainText, mutatedplainText;
+    //initialization first generation
+    mappedString = "ZXQJKVBPYGFWMUCLDRHSNIOATE"; // use most common frequencyOrderedString as innitial mappedString
+    decryptDictionary = getDecryptDictionary(sortedLetterString, mappedString);
+    plainText = decrypt(cypherText, decryptDictionary);
+    highestScore = getScore(plainText, Quadrigram);
+
+    // cout<<"current highestScore: "<<highestScore<<"\n";
+    for(int i = 0; i < repeatTime; i++){
+        mutatedMappedString = getMutatedMappedString(mappedString, mutatedMaxWidth);
+        mutatedDecryptDictionary = getDecryptDictionary(sortedLetterString, mutatedMappedString);
+        mutatedplainText = decrypt(cypherText, mutatedDecryptDictionary);
+        mutatedScore = getScore(mutatedplainText, Quadrigram);
+        if(mutatedScore > highestScore){
+            mappedString = mutatedMappedString;
+            decryptDictionary = mutatedDecryptDictionary;
+            plainText = mutatedplainText;
+            highestScore = mutatedScore;
+            // cout<<"current highestScore: "<<highestScore<<"\n";
+            // cout<<"current plainText: "<<plainText<<"\n";
+            // cout<<"current mappedString: "<<mappedString<<"\n";
+        }
+    }
+    pair<double,pair<string, string>> best;
+    best.first = highestScore;
+    best.second.first = mappedString;
+    best.second.second = plainText;
+    return best;
 }
 
 int main() {
@@ -158,37 +194,17 @@ int main() {
     }
     string cypherText = readCypherText(countMap);
     string sortedLetterString = getsortedLetterString(countMap);
-    string  mappedString = "ZXQJKVBPYGFWMUCLDRHSNIOATE", mutatedString; // use most common frequencyOrderedString as innitial mappedString
-    unordered_map<char, char> decryptDictionary, mutateddecryptDictionary;
     unordered_map<string, double> Quadrigram = getQuadrigram();
-    int highestScore = 0, mutatedScore = 0;
-    
-     
-    decryptDictionary = getDecryptDictionary(sortedLetterString, mappedString);
 
-    
-
-
-
-
-
-
-
-
-    // for(auto& pair: Quadrigram){
-    //     cout<<pair.first<<","<<pair.second<<"\n";
-    // }
-    cout<<getScore("Theinstallationsaresiteresponsivetheprocessisacollaborationwiththeimmediatecommunitiesinlocalspacestheinstallationsconsistedoftwolayersofsoundaseriesofcompositionscreatedfromlullabiescontributedbyfathersfromtheimmediatecommunityandasetofmotionactivatedsoundstationsaudiencespresencepromptsmultitudesofstoriestounfoldaudiodocumentariescreatedinconversationwithfathersonfederalprobationinstallationsareanopencalltothecommunitythroughaparticipatorylocationbasedcontributoryaudioaugmentedrealityplatformthatcanbeexperiencedonlinethroughamapandinphysicalspaceorbywalkingtheneighborhoodsasanaugmentedaudioappthemapcreatesasonicrepresentationofmemorieslullabiesvisionsanddesirescocreatingnewcollectivesocialmemories", Quadrigram)<<"\n";
-
-    for(int i = 0; i < 1000; i++){
-        getNewMappedString(mappedString,6);
-        cout<<mappedString<<"\n";
+    set<pair<double,pair<string, string>>> decrptionKeys;
+    for(int i = 0; i < processingTime; ++i){
+        decrptionKeys.insert(getNearOptimalDecrytionKey(Quadrigram, sortedLetterString, cypherText));
     }
-
+    // cout<<decrptionKeys.size()<<"\n";
+    pair<double,pair<string, string>> bestDecrptionKey;
+    bestDecrptionKey = *decrptionKeys.rbegin();
+    cout<<bestDecrptionKey.first<<": "<<bestDecrptionKey.second.first<<"\n";
+    cout<<bestDecrptionKey.second.second<<"\n";
 
     return 0;
 }
-
-
-
-
